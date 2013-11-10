617224adf1205eb39692c4ace4741d60521c7666
Jamie Curle <jamiecurle@users.noreply.github.com>
2013-11-10 16:51:22 +0000

    Update sms.py
    Oops; I put the wrong number in.

--- sms.py
+++ sms.py
@@ -14,7 +14,7 @@ except KeyError:
     sys.exit()
 
 # the recipient
-RECIPIENT = '+4478345388842'
+RECIPIENT = '+447834538884'
 
 # make the client and send the sms
 client = TwilioRestClient(account, token)
